2a723a9cdd57d971f0f6d35e5e0562107a6abcea
Alexander Furones <afurones43@gmail.com>
2019-07-18 10:05:44 -0400

    Add db path creation

--- SNAP/Configuration.cs
+++ SNAP/Configuration.cs
@@ -3,22 +3,62 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace pGina.Plugin.SNAP
 {
     public partial class Configuration : Form
     {
+        //Sqlite required datafields
+        SQLiteConnection con;
+        SQLiteCommand cmd;
+
+        //This is a path to the database with all user info
+        private static readonly string dbPath = @"C:\Program Files\pGina\Plugins\SNAP\nfc_unlock.db";
+
         public Configuration()
         {
             InitializeComponent();
         }
+        private Boolean hasDatabase()
+        {
+            return File.Exists(dbPath);
+        }
+        private void createDB()
+        {
+            string dbFolder = @"c:\Program Files\pGina\Plugins\SNAP";
+            System.IO.Directory.CreateDirectory(dbFolder);
+
+            SQLiteConnection.CreateFile(dbPath);
+
+            string sql = @"CREATE TABLE Users(
+                               UserId INTEGER PRIMARY KEY AUTOINCREMENT,
+                               DevId     TEXT  NOT NULL,
+                               UserName  TEXT  NOT NULL,
+                               PassWord  TEXT  NOT NULL,
+                               UserToken TEXT  NOT NULL,
+                               UserPin   TEXT  NOT NULL);
+                           CREATE TABLE Logs(
+                               LogId INTEGER PRIMARY KEY AUTOINCREMENT,
+                               Date TEXT NOT NULL,
+                               User TEXT NOT NULL,
+                               Message TEXT DEFAULT NULL);";
+            con = new SQLiteConnection("Data Source=" + dbPath + ";Version=3;");
+            con.Open();
+            cmd = new SQLiteCommand(sql, con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
 
         private void Configuration_Load(object sender, EventArgs e)
         {
-
+            if (!hasDatabase())
+                createDB();
+            con = new SQLiteConnection("Data Source=" + dbPath + ";Version=3;");
         }
 
         private void BtnCreateUser_Click(object sender, EventArgs e)
